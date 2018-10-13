99e07206bbcea535fdcdfbb9fea0cea8c4092836
mayinfeng <13671210359@163.com>
2018-10-13 16:22:27 +0800

    ac

--- src/easy/SortArrayByParity.java
+++ src/easy/SortArrayByParity.java
@@ -1,5 +1,30 @@
 package easy;
 
-public class SortArrayByParity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class SortArrayByParity {
+    public static int[] sortArrayByParity(int[] A) {
+        Arrays.sort(A);
+        List<Integer> old = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+        for (int i = 0;i < A.length;i++){
+            if(A[i] % 2 == 0){
+                even.add(A[i]);
+            }else{
+                old.add(A[i]);
+            }
+        }
+        even.addAll(old);
+        for(int i = 0;i < even.size();i++){
+            A[i] = even.get(i);
+        }
+        return A;
+    }
+    public static void main(String[] args){
+        int[] arr = {3,1,2,4};
+        System.out.println(Arrays.toString(sortArrayByParity(arr)));
+    }
 }
